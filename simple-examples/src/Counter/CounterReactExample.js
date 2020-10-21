5d76284342e3dfae32f9a3bf2bb4cc4afba5fe2a
Henrique Augusto <hick_97@hotmail.com>
2020-10-20 22:44:36 -0300

    feat: create an Counter component with ReactJS

---
+++ simple-examples/src/Counter/CounterReactExample.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// TODO: Make a Counter component using ReactJS';
+// Is a illustrative ReactJS used just for comparative purposals
+
+function Counter() {
+  return <h1>Im a counter!</h1>;
+}
+
+export default Counter;
