6de54a370c6278dc6752ae7754d0f1610052e250
Henrique Augusto <hick_97@hotmail.com>
2020-10-21 21:27:52 -0300

    feat: create an illustrative Counter component

--- simple-examples/src/Counter/CounterReactExample.js
+++ simple-examples/src/Counter/CounterReactExample.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { buttonStyle } from './styles/CounterStyles.bs'
 
-// TODO: Make a Counter component using ReactJS';
-// Is a illustrative ReactJS used just for comparative purposals
+// Counter component using ReactJS (for illustrative proposals)
 
 function Counter() {
-  return <h1>Im a counter!</h1>;
+  const [count, setCount] = React.useState(0);
+  const [error, setError] = React.useState("");
+
+  const handleDecrement = () => {
+    count === 0 ? 
+      setError("Count cannot be negative!") : setCount(count => count - 1 )
+  }
+
+  const handleIncrement = () => {
+    setCount(count => count + 1 )
+  }
+
+  const handleReset = () => {
+    setCount(0)
+    setError("")
+  }
+
+  return (
+    <div>
+      <p>Counter: {count}</p>
+      <button style={buttonStyle} onClick={handleIncrement}>+</button>
+      <button style={buttonStyle} onClick={handleDecrement}>-</button>
+      <button style={buttonStyle} onClick={handleReset}>
+        Reset
+      </button>
+      <span>{error}</span>
+    </div>
+  );
 }
 
 export default Counter;
